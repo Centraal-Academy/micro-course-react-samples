73d59a14da0eab05609bddc8535737bb4cc873a8
DezkaReid <elmaildeldezkareid@gmail.com>
2018-06-26 17:38:26 -0500

    react with bootstrap

--- react/src/index.js
+++ react/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MyButton from './react-materialui'
+import MyButton from './react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const appContainer = document.getElementById('app-container')
 
-ReactDOM.render(<MyButton message='Material Button' />, appContainer)
+ReactDOM.render(<MyButton message='Bootstrap Button' />, appContainer)
---
+++ react/src/react-bootstrap/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from 'reactstrap'
+
+export default function (props) {
+  return (
+    <Button color='danger'>{props.message}</Button>
+  )
+}
