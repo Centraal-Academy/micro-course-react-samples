69e6b16b4e2c781b7f3c2b4d3d2018444c3bbad6
DezkaReid <elmaildeldezkareid@gmail.com>
2018-06-25 20:34:41 -0500

    simple element with jsx

--- react/src/index.js
+++ react/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom'
-import myComponent from './react-simple'
+import myComponent from './react-jsx'
 
 const appContainer = document.getElementById('app-container')
 
---
+++ react/src/react-jsx/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+const myComponent = <h1>Hello World from JSX</h1>
+
+export default myComponent
