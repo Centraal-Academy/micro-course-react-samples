593276a3cb444f95b9a91637ae2fdd6704679c10
DezkaReid <elmaildeldezkareid@gmail.com>
2018-06-27 20:01:07 -0500

    fixed typo

--- react/src/react-conditional-rendering/index.js
+++ react/src/react-conditional-rendering/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function MyButton (props) {
   if (props.message) {
-    return <button>{this.props.message}</button>
+    return <button>{props.message}</button>
   }
   return <span>Something is missing</span>
 }
