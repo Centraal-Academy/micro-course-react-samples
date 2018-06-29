09db2dbf54577d0a125652bddc97a94383973606
DezkaReid <elmaildeldezkareid@gmail.com>
2018-06-29 17:58:07 -0500

    config test files

---
+++ react/jest.config.js
@@ -0,0 +1,13 @@
+const React = require('react')
+const Enzyme = require('enzyme')
+const Adapter = require('enzyme-adapter-react-16')
+Enzyme.configure({ adapter: new Adapter() })
+
+module.exports = {
+  rootDir: 'test',
+  testMatch: ['**/*-test.js'],
+  globals: {
+    React,
+    Enzyme
+  }
+}
