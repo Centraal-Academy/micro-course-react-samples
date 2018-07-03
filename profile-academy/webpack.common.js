c30f9e81861cabbbac661dd573cd8176839b20ab
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-02 22:20:01 -0500

    webpack common configuration

---
+++ profile-academy/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+const config = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: './index.html'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
+
+module.exports = config
