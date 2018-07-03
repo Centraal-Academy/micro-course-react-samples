4cab38f82de28c7ceed06e67f50f64d628cc924b
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-02 20:29:14 -0500

    react jsx fixed

--- react/src/react-jsx/index.js
+++ react/src/react-jsx/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
-const myComponent = <h1>Hello World from JSX</h1>
 
-export default myComponent
+export default function Title (props) {
+  return <h1>{props.message}</h1>
+}
