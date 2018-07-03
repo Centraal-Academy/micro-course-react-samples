60055d0fca1e46a63bf559242cd10f5c469cce5a
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-02 21:00:03 -0500

    server config

--- profile-academy/src/index.js
+++ profile-academy/src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom'
 
 const appElement = document.getElementById('app-container')
 
-ReactDOM.render(<h1>Hello World</h1>, appElement)
+ReactDOM.render(<h1>Hello World X2</h1>, appElement)
--- profile-academy/webpack.config.js
+++ profile-academy/webpack.config.js
@@ -1,6 +1,14 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
+  devServer: {
+    port: 8000,
+    historyApiFallback: true,
+    open: true,
+    watchOptions: {
+      ignored: /node_modules/
+    }
+  },
   module: {
     rules: [
       {
