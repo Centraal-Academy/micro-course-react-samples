18a684f68d815b8a0f00e032187d4223d5309078
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-05 17:22:56 -0500

    imported AppRouter component

--- profile-academy/src/App/index.js
+++ profile-academy/src/App/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Page from '../components/Page'
-import ProfilePage from '../pages/ProfilePage'
+import AppRouter from './AppRouter'
 
 export default class App extends React.Component {
   render () {
     return (
       <Page>
-        <ProfilePage />
+        <AppRouter />
       </Page>
     )
   }
