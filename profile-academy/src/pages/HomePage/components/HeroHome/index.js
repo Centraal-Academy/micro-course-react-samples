3c781772abba31afd7bce06d03a1eaa7abf5f53a
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-05 21:39:12 -0500

    Added navigation button

--- profile-academy/src/pages/HomePage/components/HeroHome/index.js
+++ profile-academy/src/pages/HomePage/components/HeroHome/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './index.css'
+import Button from '../../../../components/Button'
+import { withNavigation } from '../../../../components/routing'
 
 export default function HeroHome (props) {
+  const NavButton = withNavigation(Button)
   return (
     <div className='Hero-Home'>
-      <button>Go to profile</button>
+      <NavButton path='/profile'>Go to profile</NavButton>
     </div>
   )
 }
