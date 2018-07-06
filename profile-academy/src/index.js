6f04d065a17b71c7f1b75ad2072a4db0ac765b53
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-05 21:40:52 -0500

    destructured render method

--- profile-academy/src/index.js
+++ profile-academy/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import App from './App'
 import './index.css'
 
 const appElement = document.getElementById('app-container')
 
-ReactDOM.render(<App />, appElement)
+render(<App />, appElement)
