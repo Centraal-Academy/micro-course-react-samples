c7d229d706f8263c7ba5bb259301392430dadf10
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-05 21:41:17 -0500

    Added plugin spread rest operator

--- profile-academy/webpack.common.js
+++ profile-academy/webpack.common.js
@@ -11,7 +11,8 @@ const config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['react', 'env']
+            presets: ['react', 'env'],
+            plugins: ['transform-object-rest-spread']
           }
         }
       },
