1e06c7cc5165b9db2c787bbef52fae5b26e190b0
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-06 19:17:24 -0500

    Added app context

--- profile-academy/src/App/index.js
+++ profile-academy/src/App/index.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import Page from '../components/Page'
 import AppRouter from './AppRouter'
+import AppContext from './AppContext'
+import FirebaseAdapter from '../modules/FirebaseAdapter'
+
+const authFirebase = FirebaseAdapter.getAuth()
 
 export default class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      auth: false
+    }
+  }
+
+  componentDidMount () {
+    this.unSuscribe = authFirebase().onAuthStateChanged((auth) => {
+      if (auth) {
+        this.setState({ auth })
+      } else {
+        this.setState({ auth: false })
+      }
+    })
+  }
+
+  componentWillUnmount () {
+    this.unSuscribe()
+  }
+
   render () {
     return (
-      <Page>
-        <AppRouter />
-      </Page>
+      <AppContext.Provider value={{ auth: this.state.auth }} >
+        <Page>
+          <AppRouter />
+        </Page>
+      </AppContext.Provider>
     )
   }
 }
