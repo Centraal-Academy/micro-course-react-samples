39b5985d6dfeb2bdb352bde399c046e29bda5bc3
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-06 19:15:36 -0500

    Added environment variables

--- profile-academy/webpack.common.js
+++ profile-academy/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const keys = require('./keys')
 
 const config = {
   module: {
@@ -35,6 +36,7 @@ const config = {
       template: path.resolve(__dirname, 'src/index.html'),
       filename: './index.html'
     }),
+    new webpack.DefinePlugin(keys),
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin()
   ]
