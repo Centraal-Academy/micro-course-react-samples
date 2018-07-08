a529ae9ff7eb2198c5f8fd00c39878c9f4e94c1c
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-07 21:02:56 -0500

    Wrapped App router in Consumer

--- profile-academy/src/App/AppRouter/index.js
+++ profile-academy/src/App/AppRouter/index.js
@@ -2,14 +2,28 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import HomePage from '../../pages/HomePage'
 import ProfilePage from '../../pages/ProfilePage'
+import { LoginRoute, ProtectedRoute } from '../../components/GuardRouter'
+import LoginPage from '../../pages/LoginPage'
+import AppContext from '../../App/AppContext'
 
-export default function AppRouter (props) {
+function AppRouter (props) {
   return (
     <BrowserRouter>
       <React.Fragment>
         <Route exact path='/' component={HomePage} />
-        <Route path='/profile' component={ProfilePage} />
+        <LoginRoute path='/login' component={LoginPage} {...props} />
+        <ProtectedRoute path='/profile' component={ProfilePage} {...props} />
       </React.Fragment>
     </BrowserRouter>
+  )
+}
+
+export default function (props) {
+  return (
+    <AppContext.Consumer>
+      {
+        (data) => <AppRouter {...data} />
+      }
+    </AppContext.Consumer>
   )
 }
