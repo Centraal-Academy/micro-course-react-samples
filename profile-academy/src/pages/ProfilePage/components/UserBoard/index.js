0f343788c806f9139b497ce59d0bbf9c253cb4a5
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-07 21:05:34 -0500

    changed button

--- profile-academy/src/pages/ProfilePage/components/UserBoard/index.js
+++ profile-academy/src/pages/ProfilePage/components/UserBoard/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './index.css'
-import AuthButton from '../AuthButton'
 
 export default function UserBoard (props) {
   return (
@@ -11,10 +10,7 @@ export default function UserBoard (props) {
       <div>
         <p>Nombre: {props.name}</p>
         <p>Trabajo: {props.job}</p>
-        { props.auth
-          ? <button>Dame un like :3</button>
-          : <AuthButton />
-        }
+        <button>Dale like :3</button>
       </div>
     </div>
   )
